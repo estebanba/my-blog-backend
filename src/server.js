812477da647ce7b82ad5c0d780c3f1e8d11437cb
estebanba <esteban.ba@gmail.com>
2022-10-29 09:38:46 +0200

    add comment route

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    res.send(article.comments);
+    res.json(article.comments);
   } else {
     res.send("That article doesn't exist");
   }
